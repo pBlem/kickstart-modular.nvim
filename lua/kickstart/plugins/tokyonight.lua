f45c47bc072ccb0d6398b799c88ab1cf2b09b1d7
pBlem <petersblem@birman.dk>
2025-03-14 15:03:14 +0100

    Disabled tokyo-night
    Made for sometimes getting this theme and sometimes other ones

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -17,7 +17,8 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- NOTE: uncommented so that it is not set as the colorscheme
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 }
