7b02cebc25f4b996d369802d711aa92bb52d1d77
pBlem <petersblem@birman.dk>
2025-03-18 14:41:19 +0100

    Render for markdown using latex.nvim

--- lua/custom/plugins/fsharp.lua
+++ lua/custom/plugins/fsharp.lua
@@ -1,4 +1,12 @@
 return {
   'ionide/Ionide-vim',
   event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+  config = function()
+    vim.g.LanguageClient_serverCommands = {
+      fsharp = { 'dotnet', 'fsautocomplete' },
+    }
+    vim.cmd [[
+      autocmd BufNewFile,BufRead *.fs,*.fsx,*.fsi set filetype=fsharp
+    ]]
+  end,
 }
